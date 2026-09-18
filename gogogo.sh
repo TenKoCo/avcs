24aa73aa835d091fc0e9e0feaa83a2fbaad94ae5
Sieu <51594915+TenKoCo@users.noreply.github.com>
2026-09-18 17:25:42 +0700

    Create gogogo.sh

---
+++ gogogo.sh
@@ -0,0 +1,16 @@
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i termux-api
+yes | pkg i clang
+yes | pkg i python-pip
+pip install requests pytz colorama datetime logsnag pycryptodome
+export CFLAGS="-Wno-error=implicit-function-declaration"
+pkg install python-psutil -y
+pkg install python-cryptography -y
+curl -Ls "https://raw.githubusercontent.com/TenKoCo/avcs/refs/heads/main/concac.py" -o /sdcard/Download/concac.py
